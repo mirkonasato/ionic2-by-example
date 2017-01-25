8a021eaac92eb07c027825f5f3eb2b0ac724deea
Mirko Nasato <mirko@encoded.io>
2017-01-25 17:20:01 +0000

    Section 2 - Property bindings

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+  name1 = '';
+  name2 = '';
 
 }
