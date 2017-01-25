bd2f482dc4a772204b464bb1c49317c0c658408d
Mirko Nasato <mirko@encoded.io>
2017-01-25 17:41:44 +0000

    Section 5 - Using the Contacts plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contacts } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -16,6 +17,26 @@ export class HomePage {
       sum += letters.charCodeAt(i);
     }
     return sum % 101;
+  }
+
+  picContact1() {
+    Contacts.pickContact()
+      .then(contact => {
+        this.name1 = contact.displayName;
+      })
+      .catch(error => {
+        // ignore; user didn't select a contact
+      });
+  }
+
+  picContact2() {
+    Contacts.pickContact()
+      .then(contact => {
+        this.name2 = contact.displayName;
+      })
+      .catch(error => {
+        // ignore; user didn't select a contact
+      });
   }
 
 }
