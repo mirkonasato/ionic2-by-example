8f5d81fc49083abc6354a39ff3fb91c5e3be3e7c
Mirko Nasato <mirko@encoded.io>
2017-01-25 17:21:15 +0000

    Section 2 - Calculating the score

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,4 +9,13 @@ export class HomePage {
   name1 = '';
   name2 = '';
 
+  get score() {
+    const letters = (this.name1 + this.name2).toLowerCase();
+    let sum = 0;
+    for (let i = 0; i < letters.length; i++) {
+      sum += letters.charCodeAt(i);
+    }
+    return sum % 101;
+  }
+
 }
