c1ca225d56acf015b7e2ab678b6d5e148bdaaba5
Mirko Nasato <mirko@encoded.io>
2017-01-26 13:57:41 +0000

    Section 6 - Item list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+  expenses = [
+    {
+      date: '2016-01-01',
+      amount: 7.25,
+      category: 'Food',
+      description: 'Lunch'
+    },
+    {
+      date: '2016-01-02',
+      amount: 18.90,
+      category: 'Travel',
+      description: 'Train ticket'
+    },
+    {
+      date: '2016-01-02',
+      amount: 9.00,
+      category: 'Food',
+      description: 'Dinner'
+    }
+  ];
+
+  constructor(private navCtrl: NavController) { }
 
 }
